40573a73353aa494410610551278762c744ef322
Hồ Việt <138436691+tinhgom007@users.noreply.github.com>
2025-06-09 16:23:53 +0700

    final commit (1)

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,11 +1,30 @@
 import 'dart:io' show Platform;
 
+import 'package:flutter/foundation.dart';
+
+//Product URL: https://email-sourcecode-backend-951t.onrender.com
 class ApiConfig {
   static String get baseUrl {
-    return 'https://email-sourcecode-backend-951t.onrender.com'; 
+    if (kIsWeb) {
+      return 'http://localhost:3000';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:3000';
+    } else if (Platform.isIOS) {
+      return 'http://127.0.0.1:3000';
+    } else {
+      return 'http://localhost:3000';
+    }
   }
 
   static String get webSocketUrl {
-      return 'https://email-sourcecode-backend-951t.onrender.com';
+   if (kIsWeb) {
+      return 'ws://localhost:3000';
+    } else if (Platform.isAndroid) {
+      return 'ws://10.0.2.2:3000';
+    } else if (Platform.isIOS) {
+      return 'ws://127.0.0.1:3000';
+    } else {
+      return 'ws://localhost:3000';
+    }
   }
 }
